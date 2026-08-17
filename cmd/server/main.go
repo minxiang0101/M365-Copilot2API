6f914688684ca210a49b6d5ec4f08365116a8fef
HEXUXIU <hexuxiu0911@gmail.com>
2026-08-17 01:25:22 +0000

    fix: run startup token refresh async to avoid blocking HTTP listen

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -25,7 +25,7 @@ func main() {
 	s.InitM365CloudClient()
 	s.StartAutoCleanup()
 	s.StartConvCacheGC()
-	s.RefreshExpiredTokens()
+	go s.RefreshExpiredTokens() // async: don't block HTTP startup on token refresh
 	listen := "127.0.0.1:4141"
 	if v := os.Getenv("M365_LISTEN"); v != "" {
 		listen = v
